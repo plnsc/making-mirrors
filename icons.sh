e54e2ff830d74d72e4efc3de4fbb94089014ef96
Paulo Nascimento <paulornasc@gmail.com>
2025-07-27 23:44:03 -0300

    feat: replace label icon with new webp format and remove old png asset

--- icons.sh
+++ icons.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 magick originals/pe-de-acerola-16x16.xcf -resize 16x16 -background none -gravity center -extent 16x16 static/favicon.ico
 magick originals/pe-de-acerola-32x32.xcf -resize 32x32 -background none -gravity center -extent 32x32 static/favicon-32x32.ico
-magick originals/pe-de-acerola-32x32.xcf -resize 32x32 -background none -gravity center -extent 32x32 static/pe-de-acerola-32x32.png
+magick originals/pe-de-acerola-32x32.xcf -define webp:lossless=true -resize 32x32 -background none -gravity center -extent 32x32 static/pe-de-acerola-32x32.webp
