5ac6ffe419289ddc479b57d92403ecc72878c962
Paulo Nascimento <paulornasc@gmail.com>
2025-07-27 23:17:41 -0300

    fix: update label icon to use new 32x32 image and remove old icon

--- icons.sh
+++ icons.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 magick originals/pe-de-acerola-16x16.xcf -resize 16x16 -background none -gravity center -extent 16x16 static/favicon.ico
 magick originals/pe-de-acerola-32x32.xcf -resize 32x32 -background none -gravity center -extent 32x32 static/favicon-32x32.ico
-magick originals/pe-de-acerola-340x340.xcf -resize 340x340 -background none -gravity center -extent 340x340 static/pe-de-acerola.png
+magick originals/pe-de-acerola-32x32.xcf -resize 32x32 -background none -gravity center -extent 32x32 static/pe-de-acerola-32x32.png
